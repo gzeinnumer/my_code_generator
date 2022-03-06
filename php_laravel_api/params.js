62ca765c7bb91873d94ca913685993b86a3ef899
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-06 22:58:03 +0700

    read

--- php_laravel_api/params.js
+++ php_laravel_api/params.js
@@ -1,7 +1,7 @@
-let name = "MApiResponse"; //BarangJasa
-let apiName = "api_response"; //barang_jasa
-let table = "m_api_response"; //barang_java
-let column = ['id', 'method', 'end_point', 'response_number', 'title', 'message', 'created_at', 'updated_at'];
+let name = "SdEmployee"; //BarangJasa
+let apiName = "employee"; //barang_jasa
+let table = "sd_employee"; //barang_java
+let column = ['id', 'id_users', 'name', 'created_at', 'updated_at'];
 
 module.exports = {
         p1: name,
